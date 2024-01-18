744d38c09eb3162818fd188fda3c45b120e8fe0d
ganesh717 <93328727+ganesh717@users.noreply.github.com>
2024-01-18 19:04:29 +0000

    1.2
    optimization

--- Assets/MeteorPistol.cs
+++ Assets/MeteorPistol.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class MeteorPistol : MonoBehaviour
